1532520dee16f08098a2226208be0c6c1ae5d810
MaximZykin <zikinmax95@mail.ru>
2024-04-04 22:19:28 +0700

    .

--- FilmBox/Network/NetworkDataFetch.swift
+++ FilmBox/Network/NetworkDataFetch.swift
@@ -26,5 +26,23 @@ class NetworkDataFetch {
             }
         }
     }
+    
+    static func fetchMovieDetail(urlString: String, responce: @escaping (MoviesSearch?, Error?) -> ()) {
+        
+        NetworkRequest.requestDataFetch(url: urlString) { result in
+            switch result {
+            case .success(let data):
+                do {
+                    let json = try JSONDecoder().decode(MoviesSearch.self, from: data)
+                    responce (json, nil)
+                } catch let jsonError {
+                    print("Failed to decode JSON", jsonError)
+                }
+            case .failure(let error):
+                print("Error received requesting data \(error.localizedDescription)")
+                responce(nil, error)
+            }
+        }
+    }
 }
 
